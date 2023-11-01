d44370ee5d3bb032935df6d64af3c3511ed2c14f
Didi Ethmane <95219508+Ousman37@users.noreply.github.com>
2023-11-02 00:37:19 +0100

    Add Profile API interaction methods

---
+++ src/js/api/profile/profile.mjs
@@ -0,0 +1,56 @@
+import { authFetch } from "../authFetch.mjs";
+import { API_SOCIAL_URL } from "../const.mjs";
+
+const action = "/profiles/";
+const listings = "/listings/";
+const media = "/media";
+const bid = "/bids";
+
+const method = "put";
+
+export function userProfile() {
+  try {
+    const profileFetch = JSON.parse(localStorage.profile);
+    const name = profileFetch.name;
+    return name;
+  } catch {
+    null;
+  }
+}
+
+const nameMe = userProfile();
+
+export async function getProfile() {
+  const profileURL = `${API_SOCIAL_URL}${action}${nameMe}?_listings=true`;
+
+  const response = await authFetch(profileURL);
+
+  return await response.json();
+}
+
+export async function getUserListings() {
+  const ListingsURL = `${API_SOCIAL_URL}${action}${nameMe}${listings}?_seller=true&_bids=true&sort=created&sortOrder=desc`;
+
+  const response = await authFetch(ListingsURL);
+
+  return await response.json();
+}
+
+export async function putNewAvatar(newAvatar) {
+  const newAvatarURL = `${API_SOCIAL_URL}${action}${nameMe}${media}`;
+
+  const response = await authFetch(newAvatarURL, {
+    method: method,
+    body: JSON.stringify(newAvatar),
+  });
+
+  return await response.json();
+}
+
+export async function getProfileBiddingHistory() {
+  const biddingHistoryURL = `${API_SOCIAL_URL}${action}${nameMe}${bid}?_listings=true`;
+
+  const response = await authFetch(biddingHistoryURL);
+
+  return await response.json();
+}
