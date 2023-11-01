ba300588d78497cee78ecf8849a6271faefd91f2
Didi Ethmane <95219508+Ousman37@users.noreply.github.com>
2023-11-02 00:10:14 +0100

    Add login functionality

---
+++ src/js/api/auth/login.mjs
@@ -0,0 +1,93 @@
+import { API_SOCIAL_URL } from "../const.mjs";
+import * as storage from "../../storage/index.mjs";
+
+const path = "/auth/login";
+const method = "post";
+
+export async function login(profile) {
+  const toastr = window.toastr;
+
+  if (toastr) {
+    toastr.options = {
+      closeButton: true,
+      debug: false,
+      newestOnTop: false,
+      progressBar: true,
+      positionClass: "toast-bottom-right",
+      preventDuplicates: true,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: "5000",
+      extendedTimeOut: "1000",
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut"
+    };
+  } else {
+    console.error("Toastr is not available or not properly initialized.");
+    return;
+  }
+
+  const loginURL = API_SOCIAL_URL + path;
+  const body = JSON.stringify(profile);
+
+  try {
+    const response = await fetch(loginURL, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method,
+      body,
+    });
+
+    if (response.ok) {
+      const { accessToken, ...user } = await response.json();
+      storage.save("token", accessToken);
+      storage.save("profile", user);
+
+      toastr.success("You logged in successfully! Redirecting...");
+      setTimeout(function () {
+        window.location.href = `../../pages/listings`;
+      }, 1000);
+    } else {
+      // Use toastr error for error messages
+      toastr.error('Invalid email or password, try again.');
+    }
+  } catch (error) {
+    // Handle any other errors like network issues
+    toastr.error('An error occurred. Please try again later.');
+  }
+}
+
+// import { API_SOCIAL_URL } from "../const.mjs";
+// import * as storage from "../../storage/index.mjs";
+
+// const path = "/auth/login";
+// const method = "post";
+
+// export async function login(profile) {
+//   const loginURL = API_SOCIAL_URL + path;
+//   const body = JSON.stringify(profile);
+//   const response = await fetch(loginURL, {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     method,
+//     body,
+//   });
+
+//   const { accessToken, ...user } = await response.json();
+
+//   storage.save("token", accessToken);
+
+//   storage.save("profile", user);
+
+//   if (response.ok) {
+//     setTimeout(function () {
+//       window.location.href = `../../pages/listings`;
+//     }, 1000);
+//   } else {
+//     alert("Invalid email or password, try again.");
+//   }
+// }
