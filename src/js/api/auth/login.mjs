d90a91cb6b3ef31e6641a0e6947b63c884c0979b
Didi Ethmane <95219508+Ousman37@users.noreply.github.com>
2023-11-06 12:00:34 +0100

    Implement new authentication flow in login API

--- src/js/api/auth/login.mjs
+++ src/js/api/auth/login.mjs
@@ -60,34 +60,3 @@ export async function login(profile) {
   }
 }
 
-// import { API_SOCIAL_URL } from "../const.mjs";
-// import * as storage from "../../storage/index.mjs";
-
-// const path = "/auth/login";
-// const method = "post";
-
-// export async function login(profile) {
-//   const loginURL = API_SOCIAL_URL + path;
-//   const body = JSON.stringify(profile);
-//   const response = await fetch(loginURL, {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     method,
-//     body,
-//   });
-
-//   const { accessToken, ...user } = await response.json();
-
-//   storage.save("token", accessToken);
-
-//   storage.save("profile", user);
-
-//   if (response.ok) {
-//     setTimeout(function () {
-//       window.location.href = `../../pages/listings`;
-//     }, 1000);
-//   } else {
-//     alert("Invalid email or password, try again.");
-//   }
-// }
