ef4e5f90e5d1144b7d28405d111b98d6d1f834b1
Didi Ethmane <95219508+Ousman37@users.noreply.github.com>
2023-11-06 12:02:58 +0100

    Update navigation bar to include profile image

--- src/js/handlers/profileImgNav.mjs
+++ src/js/handlers/profileImgNav.mjs
@@ -2,11 +2,13 @@ import * as methods from "../api/profile/profile.mjs";
 import { renderProfileImgData } from "../templates/navImgTemplate.mjs";
 
 export async function viewProfileImg() {
-  try {
-    const userData = await methods.getProfile();
-    renderProfileImgData(userData);
-  } catch (error) {
-    console.error('Error fetching user profile:', error);
-  }
+  // Wrap the call in an event listener to ensure the DOM is fully loaded
+  document.addEventListener('DOMContentLoaded', async () => {
+    try {
+      const userData = await methods.getProfile();
+      renderProfileImgData(userData);
+    } catch (error) {
+      console.error('Error fetching user profile:', error);
+    }
+  });
 }
-
