e77028504f42ac935f95aedd6457dd417dae770c
Didi Ethmane <95219508+Ousman37@users.noreply.github.com>
2023-11-06 12:11:21 +0100

    Implement countdown UI component for auctions

--- src/js/ui/count-down.mjs
+++ src/js/ui/count-down.mjs
@@ -1,34 +1,49 @@
 export default function displayCountdown(listingData, container, parentContainer) {
-  const countDown = new Date(listingData.endsAt).getTime();
+  // Verify that listingData.endsAt is a valid date
+  if (isNaN(new Date(listingData.endsAt).getTime())) {
+    console.error('Invalid date format for listingData.endsAt');
+    return;
+  }
 
+  const countDown = new Date(listingData.endsAt).getTime();
   let interval; // Declare the interval here
 
   const updateCountdownDisplay = () => {
       const now = new Date().getTime();
       const distance = countDown - now;
 
+      // Check if the container still exists in the DOM
+      if (!document.body.contains(container)) {
+        clearInterval(interval);
+        return;
+      }
+
+      if (distance < 0) {
+          container.innerHTML = '<small class="text-danger"> Time is up! Auction ended.</small>';
+          if (parentContainer) {
+            parentContainer.classList.add("hide");
+          }
+          clearInterval(interval);
+          return;
+      }
+
       const days = Math.floor(distance / (1000 * 60 * 60 * 24));
       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      if (distance < 0) {
-          container.innerHTML = '<small class="text-danger"> Time is up! Auction ended.</small>';
-          parentContainer.classList.add("hide");
-          clearInterval(interval);
-          return;
-      }
-
-      let displayText = days + " days " + hours + "h";
-      if (days === 1) displayText = days + " day " + hours + "h " + minutes + "m";
-      if (days === 0 && hours > 0) displayText = hours + "h " + minutes + "m " + seconds + "s";
-      if (days === 0 && hours === 0 && minutes > 0) displayText = minutes + " minutes " + seconds + "s";
-      if (days === 0 && hours === 0 && minutes === 0) displayText = seconds + " seconds";
+      let displayText = `${days} day${days === 1 ? '' : 's'} ${hours}h `;
+      if (days === 0 && hours > 0) displayText = `${hours}h ${minutes}m ${seconds}s`;
+      if (days === 0 && hours === 0 && minutes > 0) displayText = `${minutes}m ${seconds}s`;
+      if (days === 0 && hours === 0 && minutes === 0) displayText = `${seconds}s`;
 
       container.innerText = displayText;
 
-      // Styling
-      container.classList.add(days === 0 && hours === 0 ? "text-danger" : "text-green", "fw-bold");
+      // Update styling
+      container.className = ''; // Reset classes
+      container.classList.add("fw-bold");
+      if (days === 0 && hours === 0) container.classList.add("text-danger");
+      else container.classList.add("text-success");
   }
 
   updateCountdownDisplay();  // Call once immediately to set initial value
